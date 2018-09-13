06a87ea5b96d5c57281932f706db0c5203eb9a3c
atm-ph <phf@atm-consulting.Fr>
2018-09-13 17:40:04 +0200

    Fix b675109 - pcq'on se retrouve avec des boutons qui disparaissent tout simplement

--- core/modules/modUpbuttons.class.php
+++ core/modules/modUpbuttons.class.php
@@ -58,7 +58,7 @@ class modUpbuttons extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Description of module Upbuttons";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.0.1';
+		$this->version = '1.0.2';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
@@ -177,15 +177,15 @@ class modUpbuttons extends DolibarrModules
 		// Add here list of permission defined by an id, a label, a boolean and two constant strings.
 		// Example:
 		$this->rights[$r][0] = $this->numero + $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = 'Utiliser le remontoir de boutons';	// Permission label
+		$this->rights[$r][1] = 'UseAllButton';	// Permission label
 		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
-		$this->rights[$r][4] = 'useit';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		$this->rights[$r][4] = 'UseAllButton';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
 		$this->rights[$r][0] = $this->numero + $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = 'UseAllButton';	// Permission label
+		$this->rights[$r][1] = 'UseSingleButton';	// Permission label
 		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
-		$this->rights[$r][4] = 'UseAllButton';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		$this->rights[$r][4] = 'UseSingleButton';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
 
--- js/upbuttons.js.php
+++ js/upbuttons.js.php
@@ -4,21 +4,18 @@
 	
 	require '../config.php';
 
-	if(empty($user->rights->upbuttons->useit)) exit;
+	if(empty($user->rights->upbuttons->UseAllButton) && empty($user->rights->upbuttons->UseSingleButton)) exit;
 
 	$langs->load('upbuttons@upbuttons')
 	
 ?>$(document).ready(function() {
 
-  var $el = $('div.tabsAction').first();
-
+  var $upbuttons_container = $('div.tabsAction').first();
+  window.setTimeout(getButtonInBanner,300); //delai for js button
   <?php
-  	if(!empty($user->rights->upbuttons->UseAllButton)) {
-  		?>
-  		window.setTimeout(getButtonInBanner,300); //delai for js button
-  		<?php
-  		//echo '$("body").append("'.addslashes('<a href="javascript:;" id="justOneButton" style="display:none;">'.img_picto('','all@upbuttons').'</a>').'");';
-  	}
+	if (!empty($user->rights->upbuttons->UseSingleButton)) {
+		echo '$("body").append("'.addslashes('<a href="javascript:;" id="justOneButton" style="display:none;">'.img_picto('','all@upbuttons').'</a>').'");';
+	}
   ?>
   
   
@@ -31,7 +28,7 @@
 	  	  if((scrollTop + wHeight < originalElementTop) || (scrollLeft + wWidth < originalElementLeft)) {
 	  	  	//console.log("tabsAction not in screen ");
 	  	  	
-	  	  	$el.css({
+	  	  	$upbuttons_container.css({
 	  	  		position:"fixed"
 	  	  		,bottom:'-1px'
 	  	  		,right:'-1px'
@@ -43,27 +40,29 @@
 	  	  		,'opacity':1
 	  	  	});
 	  	  	
-	  	  	$el.addClass('upbuttonsdiv');
+	  	  	$upbuttons_container.addClass('upbuttonsdiv');
 
 			$('#justOneButton').click(function() {
 				
-				if($el.is(":visible")) {
-					$el.hide();
+				if($upbuttons_container.is(":visible")) {
+					$upbuttons_container.hide();
 					$(this).css('bottom', 20);
 					
 				}
 				else {
-					$el.show();
-					$(this).css('bottom', $el.height() + 10);
+					$upbuttons_container.show();
+					$(this).css('bottom', $upbuttons_container.height() + 10);
 					
 				}
 			});
 			
 										
 	  	  	<?php
-  				if(!empty($user->rights->upbuttons->UseAllButton)) {
+  				if( empty($user->rights->upbuttons->UseAllButton)
+					&& !empty($user->rights->upbuttons->UseSingleButton)
+				) {
   					?>
-			  	  	$el.hide();
+			  	  	$upbuttons_container.hide();
 			  	  	$('#justOneButton').css({
 			  	  		position:"fixed"
 			  	  		,bottom:'20px'
@@ -73,17 +72,14 @@
 			  	  	}).show();
 			  	  	
 			  	  	<?php
-				}
-				else {
-					null;		
 				}	
 			 ?>
 	  	  }	
 	  	  else{
 	  	  	//console.log("tabsAction in screen ");
-	  	  	$el.removeAttr('style');
-	  	  	$el.removeClass('upbuttonsdiv');
-			$el.show();
+	  	  	$upbuttons_container.removeAttr('style');
+	  	  	$upbuttons_container.removeClass('upbuttonsdiv');
+			$upbuttons_container.show();
 			$('#justOneButton').hide();
 	  	  }
   }
@@ -91,13 +87,13 @@
   var editline_subtotal = -1; /* .indexOf returns -1 if the value to search for never occurs */
   if (typeof referer !== 'undefined') editline_subtotal  = referer.indexOf('action=editlinetitle');
   
-  if (editline_subtotal == -1 && ($el.length == 1 && ($el.find('.button').length>0 || $el.find('.butAction').length>0)))
+  if (editline_subtotal == -1 && ($upbuttons_container.length == 1 && ($upbuttons_container.find('.button').length>0 || $upbuttons_container.find('.butAction').length>0)))
   {
-  		var originalElementTop = $el.offset().top;
-		var originalElementLeft = $el.offset().left;
+  		var originalElementTop = $upbuttons_container.offset().top;
+		var originalElementLeft = $upbuttons_container.offset().left;
 		
 		if(originalElementTop <= 0) {
-			window.setTimeout(function() { originalElementTop = $el.offset().top;originalElementLeft = $el.offset().left; scrollButtonsToUp(); },100);
+			window.setTimeout(function() { originalElementTop = $upbuttons_container.offset().top;originalElementLeft = $upbuttons_container.offset().left; scrollButtonsToUp(); },100);
 		}
 		$( window ).resize(function() {      	  
 			scrollButtonsToUp();
@@ -114,8 +110,8 @@
  });
   
 function getButtonInBanner() {
-  var $el = $('div.tabsAction').first();
-  if($el.length == 0 ) return;
+  var $upbuttons_container = $('div.tabsAction').first();
+  if($upbuttons_container.length == 0 ) return;
 
   $('div.fiche div.pagination').css('padding',0);
   $('div.fiche div.statusref').css('margin-bottom','8px');
@@ -125,7 +121,7 @@ function getButtonInBanner() {
   $ul = $('<ul></ul>');
   $ul.hide();
   
-  $el.find('a,#action-clone').each(function(i,item) {
+  $upbuttons_container.find('a,#action-clone').each(function(i,item) {
     $item = $(item);
     var $a = $item.clone(true, true);
     
