1985f17fc5e8a8f9187a680f795c588340921eea
ATM john <john.botella@atm-consulting.fr>
2020-06-28 23:02:25 +0200

    Add sticky tab

--- core/modules/modUpbuttons.class.php
+++ core/modules/modUpbuttons.class.php
@@ -97,7 +97,7 @@ class modUpbuttons extends DolibarrModules
 		$this->dirs = array();
 
 		// Config pages. Put here list of php page, stored into upbuttons/admin directory, to use to setup module.
-		$this->config_page_url = false;
+		$this->config_page_url = array('upbuttons_setup.php@upbuttons');
 
 		// Dependencies
 		$this->hidden = false;			// A condition to hide module
@@ -255,7 +255,7 @@ class modUpbuttons extends DolibarrModules
 	function init($options='')
 	{
 		$sql = array();
-		
+
 		define('INC_FROM_DOLIBARR',true);
 
 		dol_include_once('/upbuttons/config.php');
